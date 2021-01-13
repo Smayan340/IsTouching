547319646be52eb7d0831b331d441ede5e9de80a
Smayan340 <76809078+Smayan340@users.noreply.github.com>
2021-01-13 15:14:39 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,26 +1,65 @@
+var fixedRect, movingRect;
+var gameObject1, gameObject2, gameObject3, gameObject4;
+
 function setup() {
-   createCanvas(800,400);
-    fixedRect = createSprite(200, 200, 50, 80);
-     fixedRect.shapeColor = "green";
-      movingRect = createSprite(100, 20, 80, 30);
-       movingRect.shapeColor = "green";
-       }
-      
-  function draw() {
- background("black");
- movingRect.x = World.mouseX;
- movingRect.y = World.mouseY;
- if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2 
- && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
- &&movingRect.y - fixedRect.y < fixedRect.width/2 + movingRect.width/2 
- && fixedRect.y - movingRect.y < fixedRect.width/2 + movingRect.width/2) 
- { 
- fixedRect.shapeColor = "red";
-  movingRect.shapeColor = "red";
-} 
-else { movingRect.shapeColor = "green";
- fixedRect.shapeColor = "green"; 
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
  
+  movingRect = createSprite(800, 400,80,30);
+  movingRect.shapeColor = "green";
+
+  gameObject1 = createSprite(100, 100, 50, 50);
+  gameObject1.shapeColor = "green";
+  gameObject2 = createSprite(200, 100, 50, 50);
+  gameObject2.shapeColor = "green";
+  gameObject3 = createSprite(300, 100, 50, 50);
+  gameObject3.shapeColor = "green";
+  gameObject4 = createSprite(400, 100, 50, 50);
+  gameObject4.shapeColor = "green";
 }
-drawSprites();
+
+function draw() {
+  background(0,0,0);  
+  
+  
+  movingRect.velocityX=-5;
+  fixedRect.velocityX=+5;
+
+  if(isTouching(movingRect, gameObject1)){
+    movingRect.shapeColor = "blue";
+    gameObject1.shapeColor = "blue";
+  }
+  else {
+    movingRect.shapeColor = "green";
+    gameObject1.shapeColor = "green";
+  }
+bounceOff(fixedRect,movingRect);
+
+
+
+
+  drawSprites();
 }
+
+function isTouching(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object2.y < object2.height/2 + object1.height/2) {
+    
+    return true;
+  }
+  else {
+    return false;
+  } 
+}
+
+function bounceOff(object1,object2){
+if (object1.x - object2.x < object2.width/2 + object1.width/2
+ && object2.x - object1.x < object2.width/2 + object1.width/2)
+{ object1.velocityX = object1.velocityX * (-1); object2.velocityX = object2.velocityX * (-1); }
+ if (object1.y - object2.y < object2.height/2 + object1.height/2 
+ && object2.y - object2.y < object2.height/2 + object1.height/2)
+{ object1.velocityY = object1.velocityY * (-1); object2.velocityY = object2.velocityY * (-1); } }
+
